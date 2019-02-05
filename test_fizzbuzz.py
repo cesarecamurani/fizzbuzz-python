7d27c05537e2cc5de0dcea33de6b2ea8004ee4a6
Cesare Camurani <cesarecamurani@Eleni-PC.home>
2019-02-05 14:47:34 +0000

    test for returning "fizzbuzz" passed

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -8,5 +8,10 @@ class TestFizzBuzz(unittest.TestCase):
         self.assertEqual(FizzBuzz(4), 4)
         self.assertEqual(FizzBuzz(7), 7)
 
+    def test_multiple_15_should_return_fizzbuzz(self):
+        self.assertEqual(FizzBuzz(45), "fizzbuzz")
+        self.assertEqual(FizzBuzz(30), "fizzbuzz")
+        self.assertEqual(FizzBuzz(60), "fizzbuzz")
+
 if __name__ == '__main__':
     unittest.main()
